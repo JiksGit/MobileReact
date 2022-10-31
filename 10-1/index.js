aba95f4ea29232c927c7a3c22a9fd0506b4fdf20
hyunjik Yang <2wlrlwkd@gmail.com>
2022-10-31 16:08:28 +0900

    Context
    10/31

---
+++ 10-1/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ContextHook from './chapter_10/ContextHook.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+        <ContextHook />
+);
